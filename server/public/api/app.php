218bd7f52126616e74c6e95319ce532fc0e0a035
Kevin Yang <kyang812@yahoo.com>
2019-05-31 13:34:36 -0700

    added basic error handling functionality, added functions,php, added health_check.php

--- server/public/api/app.php
+++ server/public/api/app.php
@@ -1,11 +1,18 @@
 <?php
-  require_once('dbconnection.php');
+require_once 'db_connection.php';
+require_once 'functions.php';
+set_exception_handler("error_handler");
+startUp();
 
-  $query = 'SELECT * FROM `dummy`';
-  $result = mysqli_query( $conn , $query );
+if ( !$conn ){
+  throw new Exception( 'Error: ' . mysqli_connect_error( $conn ));
+}
 
-  while ( $row = mysqli_fetch_assoc( $result )){
-    $output[] = $row;
-  }
-  print_r( json_encode($output ) );
+$query = 'SELECT * FROM `dummy`';
+$result = mysqli_query( $conn , $query );
+
+while ( $row = mysqli_fetch_assoc( $result )){
+  $output[] = $row;
+}
+print_r( json_encode($output ) );
 ?>
--- server/public/api/profile.php
+++ server/public/api/profile.php
@@ -1,9 +1,12 @@
 <?php
+
+header("Content-Type:application/json");
+
 $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
 
 if ($method === 'POST'){
-    header("Content-Type:application/json");
+    
     print($item);
 }
 elseif ($method === 'GET'){
