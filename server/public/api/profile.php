0616be776e3ce6b3ef7d4f7cef49160f47cb3851
uncosung <eric.k.sung@gmail.com>
2019-06-01 19:46:29 +0000

    Merged back end to send data to view profile fe

--- server/public/api/profile.php
+++ server/public/api/profile.php
@@ -6,6 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
 
 if ($method === 'POST'){
+    session_start();
     $output = json_decode($item, true);
     if ($output['isGuide'] === false){
         $output['isGuide'] = 0;
@@ -16,9 +17,24 @@ if ($method === 'POST'){
     $query = "INSERT INTO `profile`(`name`, `email`, `location`, `bio`, `image`, `isGuide`) 
     VALUES ('{$output['name']}', '{$output['email']}', '{$output['location']}', '{$output['bio']}', '{$output['image']}', '{$output['isGuide']}')";
     $result = mysqli_query($conn, $query);
+    $_SESSION['userEmail'] = $output['email'];
     print_r($result);
 }
 elseif ($method === 'GET'){
-    readfile('dummy-profile.json');
+    $email = $_GET['email'];
+    $query = "SELECT
+    `profile`.`name`, `profile`.`location`
+    FROM `profile`
+    WHERE `profile`.`email` = '{$email}'";
+
+    $result = mysqli_query($conn, $query);
+    $output = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $output['name'] = $row['name'];
+        $output['location'] = $row['location'];
+    }
+    $_SESSION['userEmail'] = $email;
+    $json_output = json_encode($output);
+    print_r($json_output);
 }
 ?>
