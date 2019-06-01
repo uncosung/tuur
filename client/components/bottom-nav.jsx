b8ef79d097dbc3cfe343fc5bd2eae7f9bab4907b
kate park <rudhi1009@gmail.com>
2019-06-01 15:03:43 -0700

    make color theme and bottom nav sticks on bottom

--- client/components/bottom-nav.jsx
+++ client/components/bottom-nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Home from '@material-ui/icons/Home';
@@ -8,19 +8,26 @@ import CardTravel from '@material-ui/icons/CardTravel';
 import Message from '@material-ui/icons/Message';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
-// const useStyles = makeStyles({
-//     stickToBottom: {
-//         width: '100%',
-//         position: 'fixed',
-//         bottom: 0,
-//       },
-// });
+const useStyles = makeStyles({
+  root: {
+    width: '90%',
+    position: 'fixed',
+    bottom: 0,
+    padding: 10
+  }
+});
 
 function BottomNav() {
-//   const classes = useStyles();
+  const classes = useStyles();
+  const [value, setValue] = React.useState('home');
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+    console.log('nav clicked', newValue); // newValue is index 0-5
+  }
 
   return (
-    <BottomNavigation showLabels>
+    <BottomNavigation showLabels value={value} onChange={handleChange} className={classes.root}>
       <BottomNavigationAction label="Home" icon={<Home />} />
       <BottomNavigationAction label="Favorites" icon={<FavoriteBorder />} />
       <BottomNavigationAction label="Itinerary" icon={<CardTravel />} />
@@ -30,5 +37,4 @@ function BottomNav() {
   );
 }
 
-// export default makeStyles(useStyles)(BottomNav);
 export default BottomNav;
--- server/public/api/profile.php
+++ server/public/api/profile.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'dbconnection.php';
+require_once 'db_connection.php';
 header("Content-Type:application/json");
 $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
