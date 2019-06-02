bfeaea3360417c7610d5430e3dfafe5c4a58972e
kate park <rudhi1009@gmail.com>
2019-06-01 17:44:40 -0700

    grid list for upcoming turrs

--- client/components/user-upcoming-tuurs-list-item.jsx
+++ client/components/user-upcoming-tuurs-list-item.jsx
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import LinesEllipsis from 'react-lines-ellipsis';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    margin: theme.spacing(1)
-  },
-  gridList: {
-    flexWrap: 'nowrap',
-    transform: 'translateZ(0)',
-    margin: theme.spacing(2),
-    height: 310
-  },
-  marginTop: {
-    marginTop: theme.spacing(3)
-  },
-  avatar: {
-    width: 80,
-    height: 80
-  },
-  marginLeft: {
-    marginLeft: theme.spacing(2)
-  },
-  media: {
-    height: 200
+  tile: {
+    width: 300
   },
   font: {
     fontFamily: 'Roboto',
@@ -52,25 +26,18 @@ class UpComingTuurItem extends Component {
   }
 
   render() {
-    console.log(this.props.package);
     const { classes } = this.props;
     return (
       <>
           <GridListTile className={classes.font} key={this.props.package.mainImage}>
-            <img className=""src={this.props.package.mainImage} alt={this.props.package.title} />
+            <img className={classes.tile} src={this.props.package.mainImage} alt={this.props.package.title} />
             <GridListTileBar
               title={this.props.package.title}
+              subtitle={<span>{this.props.package.description}</span>}
               classes={{
                 root: classes.titleBar
               }}
             />
-            {/* <LinesEllipsis
-              text = {this.props.package.description}
-              maxLine='3'
-              ellipsis='...'
-              trimRight
-              basedOn='letters'
-            /> */}
           </GridListTile>
       </>
     );
