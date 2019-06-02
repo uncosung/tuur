fae52c3461db541d7bcb75f2aea34a9217a11d8d
jenOng19 <jennifer.sy.ong@gmail.com>
2019-06-01 17:47:20 -0700

    worked on created package page added input field added helpertext for errors added filter tags

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,15 +3,17 @@ import UserProfile from './user-profile';
 import EditProfile from './user-edit-profile';
 import SignUp from './sign-up';
 import BottomNav from './bottom-nav';
+import CreatePackage from './createPackage'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <EditProfile/>
+        {/* <EditProfile/> */}
         {/* <UserProfile/> */}
         {/* <SignUp /> */}
-        <BottomNav />
+        {/* <BottomNav /> */}
+        <CreatePackage />
       </div>
     );
   }
