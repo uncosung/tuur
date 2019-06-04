e7deeff8f5fd0d70609f18cb4748924fb2b0761c
uncosung <eric.k.sung@gmail.com>
2019-06-04 22:59:15 +0000

    added mapbox component, will need to be connected to rest of app

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import MapBox from './components/mapbox';
 
 ReactDOM.render(
-  <App />,
+  <MapBox />,
   document.getElementById('root')
 );
