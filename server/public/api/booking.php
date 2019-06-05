8599f184551af225cb0913adf9a2d4bd5fb4dc65
Kevin Yang <kyang812@yahoo.com>
2019-06-05 16:56:10 -0700

    replaced dummy data fetch with $output, waiting on booking functionality to test query string

--- server/public/api/booking.php
+++ server/public/api/booking.php
@@ -4,11 +4,15 @@ session_start();
 header("Content-Type:application/json");
 $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
+$email = $_SESSION['userEmail'];
+
 if ( $method === "POST"){
-  $stringQuery = json_decode( $item , true );
-  // DUMMY QUERY
+  $output = json_decode( $item , true );
+  // TEST OUTPUT ONCE BOOKING FUNCTIONALITY CREATED
+  // var_dump( $output );
+  
   $query = "INSERT INTO `booking` (`id`, `tuuristId`, `packageId`, `bookedAt`, `dates`, `tuuristEmail`) 
-            VALUES (NULL, '3', '52 ', CURRENT_TIMESTAMP, '[\"2019-06-11T07:00:00.000Z\"]', 'creed@dundermifflin.com')";
+            VALUES (NULL, '{$output['tuuristId']}', '{$output['packageId']} ', CURRENT_TIMESTAMP, '{$output['dates']}', {$email})";
   $result = mysqli_query( $conn, $query );
   print_r( json_encode( ['auth' => $result]));
 }
@@ -19,13 +23,14 @@ if ( $method === "GET"){
   FROM `package` AS pack
   JOIN `profile` AS pro ON `pro`.email = `pack`.profileEmail
   JOIN `booking` AS book ON `book`.packageId = `pack`.id
-  WHERE `book`.tuuristId = 3";
+  WHERE `book`.tuuristEmail = '{$email}'";
 
   $result = mysqli_query( $conn , $query );
   $output = [];
 
   while ( $row = mysqli_fetch_assoc( $result) ){
     $output[] = $row;
+    // $output[0]['dates'] = (substr(json_decode( $row['dates'] )[0], 0 , 10));
   }
 
   print_r( json_encode( $output ));
