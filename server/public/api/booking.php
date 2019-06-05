e734a1b245ba625cb0f3abc187ec3b8ac838b777
Kevin Yang <kyang812@yahoo.com>
2019-06-05 15:48:47 -0700

    query test

--- server/public/api/booking.php
+++ server/public/api/booking.php
@@ -5,7 +5,21 @@ header("Content-Type:application/json");
 $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
 if ( $method === "POST"){
+  // DUMMY QUERY
+  // $query = INSERT INTO `booking` (`id`, `tuuristId`, `packageId`, `bookedAt`, `dates`, `tuuristEmail`) 
+  //          VALUES (NULL, '3', '153', CURRENT_TIMESTAMP, '[\"2019-06-11T07:00:00.000Z\"]', 'creed@dundermifflin.com');
 
+}
+if ( $method === "GET"){
+  $query = "SELECT "
+
+  // package ( title, description, tags , location, timeRange, dates, mainImage , images, guideEmail )
+  // booking ( touristEmail from tuuristId, guideImage from packageId -> package -> profile table )
+  "SELECT * FROM `package` AS pack
+  JOIN `profile` AS pro 
+  ON `pro`.email = `pack`.profileEmail
+  WHERE `pack`.profileEmail = 'dPaschal@gmail.com'"
+  
 }
 
 ?>
