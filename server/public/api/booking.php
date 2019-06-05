79561d23525b0a5513508b97c999aacaa3f1ed2d
Kevin Yang <kyang812@yahoo.com>
2019-06-05 16:28:40 -0700

    connected datapoint get and post on booking.php

--- server/public/api/booking.php
+++ server/public/api/booking.php
@@ -5,21 +5,30 @@ header("Content-Type:application/json");
 $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
 if ( $method === "POST"){
+  $stringQuery = json_decode( $item , true );
   // DUMMY QUERY
-  // $query = INSERT INTO `booking` (`id`, `tuuristId`, `packageId`, `bookedAt`, `dates`, `tuuristEmail`) 
-  //          VALUES (NULL, '3', '153', CURRENT_TIMESTAMP, '[\"2019-06-11T07:00:00.000Z\"]', 'creed@dundermifflin.com');
-
+  $query = "INSERT INTO `booking` (`id`, `tuuristId`, `packageId`, `bookedAt`, `dates`, `tuuristEmail`) 
+            VALUES (NULL, '3', '52', CURRENT_TIMESTAMP, '[\"2019-06-11T07:00:00.000Z\"]', 'creed@dundermifflin.com')";
+  $result = mysqli_query( $conn, $query );
+  print_r( json_encode( ['auth' => $result]));
 }
 if ( $method === "GET"){
-  $query = "SELECT "
-
-  // package ( title, description, tags , location, timeRange, dates, mainImage , images, guideEmail )
-  // booking ( touristEmail from tuuristId, guideImage from packageId -> package -> profile table )
-  SELECT * 
+  $query = "SELECT `book`.id, `pack`.title, `pack`.description, `pack`.tags, 
+  `pack`.location, `pack`.timeRange, `pack`.mainImage, `pack`.images, `pack`.profileEmail AS guideEmail,
+  `book`.tuuristEmail, `book`.dates, `pro`.image AS guideImage
   FROM `package` AS pack
-  JOIN `profile` AS pro ON pro.email = pack.profileEmail
-  JOIN `booking` AS book ON book.tuuristId = pro.id
-  WHERE pack.profileEmail = 'dPaschal@gmail.com'
+  JOIN `profile` AS pro ON `pro`.email = `pack`.profileEmail
+  JOIN `booking` AS book ON `book`.packageId = `pack`.id
+  WHERE `book`.tuuristId = 3";
+
+  $result = mysqli_query( $conn , $query );
+  $output = [];
+
+  while ( $row = mysqli_fetch_assoc( $result) ){
+    $output[] = $row;
+  }
+
+  print_r( json_encode( $output ));
 }
 
 ?>
