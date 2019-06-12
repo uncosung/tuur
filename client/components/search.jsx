196e706588d1c4a52ecf730d84b667aee8e1f9f0
kate park <rudhi1009@gmail.com>
2019-06-12 16:35:40 -0700

    fixed carousel

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -112,7 +112,7 @@ class Search extends Component {
       dots: true,
       infinite: true,
       slidesToShow: 2,
-      slidesToScroll: 3,
+      slidesToScroll: 1,
       autoplay: true,
       speed: 2000,
       autoplaySpeed: 4000,
